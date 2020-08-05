9da64552e80f0815da7873b5fb1f3bfebe39bb3b
Eth Chonlasai <o_nongearth_o@hotmail.com>
2020-08-06 00:40:12 +0700

    test console.log

--- controllers/DOSCG.js
+++ controllers/DOSCG.js
@@ -1,36 +1,85 @@
 const request = require('request')
 const keys = require('../config')
 
+// controller
 const lineNotification = (req, res) => {
-    let event = req.body.events[0]
-    let replyToken = event.replyToken
-    let userMessage = event.message.text
-    console.log(userMessage)
+    const event = req.body.events[0]
+    const replyToken = event.replyToken
+    const userMessage = event.message.text
     reply(replyToken, userMessage)
     res.sendStatus(200)
 }
 
-function reply(replyToken, userMessage) {
-    let headers = {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${keys.channelToken}`
+const reply = (replyToken, userMessage) => {
+    let responseMessage = null
+    switch(userMessage) {
+        case 'X Y Z Finding value':
+            const {x, y, z} = XYZFinding()
+            responseMessage = `X = ${x}, Y = ${y}, Z = ${z}`
+        case 'B C Finding value':
+            // B C Finding Function
+        case 'SCG Bangsue to Central World':
+            // Google API Function
+        default:
+            responseMessage = userMessage
     }
-    let body = JSON.stringify({
+    const body = JSON.stringify({
         replyToken: replyToken,
         messages: [
             {
                 type: 'text',
-                text: userMessage
+                text: responseMessage
             }
         ]
     })
     request.post({
         url: 'https://api.line.me/v2/bot/message/reply',
-        headers: headers,
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${keys.channelToken}`
+        },
         body: body
     }, (err, res, body) => {
         console.log('status = ' + res.statusCode);
     });
+}
+
+const XYZFinding = () => {
+    const set = [null, null, 5, 9, 15, 23, null]
+    const result = set.map((num, index) => {
+        return (index+1)**2+(index+1)*(-3)+5
+    })
+    const x = result[0]
+    const y =  result[1]
+    const z = result[6]
+    return {
+        x,y,z
+    }
+    // const equationSet = []
+    // const base = 1
+    // function calculate(set, base, equationSet) {
+    //     const isNumber = set.map((num, index) => {
+    //         if (num !== null) {
+    //             return {
+    //                 number: num,
+    //                 index: index+1,
+    //                 equation: `${(index**2)+(index*(-3)+5)}`
+    //             }
+    //         }
+    //     })
+    // }
+
+    function setEquation(number) {
+        return 
+    }
+    // const data = {
+    //     arr: set.reverse().map((_, index) => {
+    //         if (set[index] !== null && set[index+1] !== null) {
+    //             let result = set[index] - set[index+1]
+    //             return result
+    //         }
+    //     })
+    // }
 }
 
 module.exports = {
